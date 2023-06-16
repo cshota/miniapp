e019dd37a304dad8dd5d2ec42039a76245fa53eb
putin-futin <kawabe.futa.20010629@gmail.com>
2023-06-16 13:25:39 +0900

    g

--- 1A_TEST/src/pages/home/home.vue
+++ 1A_TEST/src/pages/home/home.vue
@@ -1,13 +1,32 @@
 <template>
+<<<<<<< Updated upstream
   <view class="content">
     <image class="logo" src="/static/soslogo.png" />
     <hello-world :title="title" />
   </view>
+=======
+  
+  <page-navbar :current="0" @clickBarItem="redirectToByIndex" />
+  
+  
+  
+>>>>>>> Stashed changes
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-const title = ref("Hello Vue");
+import { onLoad } from "@dcloudio/uni-app";
+const title = ref("sosアプリ");
+
+const swiperLoading = ref<boolean>(false);
+const loading = ref<boolean>(false);
+
+const redirectToByIndex = (path: string) => {
+  uni.reLaunch({
+    url: path,
+  });
+};
+
 </script>
 
 <style>
@@ -18,12 +37,13 @@ const title = ref("Hello Vue");
   justify-content: center;
 }
 
-.logo {
-  height: 290rpx;
-  width: 290rpx;
-  margin-top: 200rpx;
+.soslogo {
+  height: 270rpx;
+  width: 270rpx;
+  margin-top: 100rpx;
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 50rpx;
 }
 </style>
+
