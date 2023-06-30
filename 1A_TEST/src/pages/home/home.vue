dcf18a2d332664eaa4f844f50bfadea28e545008
putin-futin <kawabe.futa.20010629@gmail.com>
2023-06-30 14:26:27 +0900

    見た目

--- 1A_TEST/src/pages/home/home.vue
+++ 1A_TEST/src/pages/home/home.vue
@@ -1,18 +1,23 @@
 <template>
-  <!-- ナビゲーションバー -->
-  <view style="height:100px"></view>
-  <router-link to="./search.vue" active-class="selected-link">災害</router-link>
-  <router-view></router-view>
-  <router-link to="./plus.vue" active-class="selected-link">防災</router-link>
-  <page-navbar />
-  <page-searchbar />
+  <div class="container">
+    <!-- ナビゲーションバー -->
+    <div class="navbar">
+      <div class="navbar-link">
+        <router-link to="./search.vue" active-class="selected-link">災害</router-link>
+      </div>
+      <div class="divider"></div>
+      <div class="navbar-link">
+        <router-link to="./plus.vue" active-class="selected-link">防災</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+    <page-navbar />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import PageSearchbar from '../../components/page-searchbar/page-searchbar.vue';
 import PageNavbar from '../../components/page-navbar/page-navbar.vue'
-
 
 const redirectToByIndex = () => {
   alert();
@@ -21,19 +26,40 @@ const redirectToByIndex = () => {
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+.container {
+  padding-top: 0;
+  padding-bottom: 0;
 }
 
-.logo {
-  height: 290rpx;
-  width: 290rpx;
-  margin-top: 200rpx;
-  margin-left: 0;
-  margin-right: 0;
-  margin-bottom: 50rpx;
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f2f2f2;
+  padding: 10px;
 }
+
+.navbar-link {
+  display: flex;
+  align-items: center;
+  margin: 0 100px;
+}
+
+.navbar-link a {
+  text-decoration: none;
+  color: #333;
+  padding: 5px;
+}
+
+.divider {
+  width: 1px;
+  height: 100%;
+  background-color: #333;
+  margin: 0 100px;
+}
+
+.selected-link {
+  font-weight: bold;
+}
+
 </style>
